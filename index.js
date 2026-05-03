f49bc41fc6676fea912c7c1cfde83fadd2bace6a
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-05-03 12:55:01 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+function combineUsers(...args) {
+	const combinedObject = { users: [] };
 
+	args.forEach((element) => {
+		combinedObject.users.push(...element);
+	});
 
+	const now = new Date();
+	combinedObject.merge_date = now.toLocaleDateString("en-US", {
+		month: "numeric",
+		day: "numeric",
+		year: "numeric",
+	});
+
+	return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+	...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
